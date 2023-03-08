36231027544806240efcb8ee5bc0f0d982de4c26
ecanvasser <eddiecanvassere9@gmail.com>
2023-03-07 23:59:50 -0800

    Change layout structure for Home and child pages

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
 import { Inter } from "next/font/google";
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function HomePage() {
-  return <main className="text-2xl">HomePage</main>;
+  return (
+    <main className="text-2xl flex flex-col items-center justify-center gap-24 h-screen bg-black text-white">
+      HomePage
+      <Link href="/dashboard" className="text-lg text-black bg-white rounded px-2 py-1">Browse Companies</Link>
+    </main>
+  )
 }
---
+++ app/profile/layout.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Profile",
+};
+
+export default function layout({ children }) {
+  return (
+    <>
+      <nav className="flex justify-between py-6 px-8 text-white bg-black">
+        <div>Greenhouse Hunter</div>
+        <ul className="flex gap-6">
+          <li><Link href="/dashboard">Dashboard</Link></li>
+          <li><Link href="/profile">Profile</Link></li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
